67cdfb0399aad4b42307ebaf3743c994c7489547
lyashenkoav92@gmail.com <andreiliashenko@users.noreply.github.com>
2015-04-02 21:17:04 +0000

    Multilevel inheritance support is added to base class extractor

--- src/main/java/com/anli/busstation/dal/jpa/extractors/BaseJoinExtractor.java
+++ src/main/java/com/anli/busstation/dal/jpa/extractors/BaseJoinExtractor.java
@@ -7,24 +7,64 @@ import javax.persistence.Table;
 import org.eclipse.persistence.descriptors.ClassExtractor;
 import org.eclipse.persistence.sessions.Record;
 import org.eclipse.persistence.sessions.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class BaseJoinExtractor extends ClassExtractor {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BaseJoinExtractor.class);
+
     protected Map<Class, String> rowNames;
+    protected Map<Class, BaseJoinExtractor> childrenExtractors;
 
     public BaseJoinExtractor() {
         rowNames = null;
+        childrenExtractors = null;
     }
 
     @Override
     public Class extractClassFromRow(Record databaseRow, Session session) {
         for (Map.Entry<Class, String> rowName : getRowNames().entrySet()) {
             if (databaseRow.get(rowName.getValue()) != null) {
-                return rowName.getKey();
+                Class clazz = rowName.getKey();
+                BaseJoinExtractor extractor = getExtractor(clazz);
+                return extractor != null ? extractor.extractClassFromRow(databaseRow, session)
+                        : clazz;
             }
         }
         throw new RuntimeException("Could not determine entity type for record :"
                 + databaseRow.toString());
+    }
+
+    protected void addChildrenExtractor(Class clazz) {
+        org.eclipse.persistence.annotations.ClassExtractor extractor =
+                (org.eclipse.persistence.annotations.ClassExtractor) clazz
+                .getAnnotation(org.eclipse.persistence.annotations.ClassExtractor.class);
+        if (extractor == null) {
+            return;
+        }
+        Class extractorClass = extractor.value();
+        if (!BaseJoinExtractor.class.isAssignableFrom(extractorClass)) {
+            return;
+        }
+        BaseJoinExtractor extractorInstance;
+        try {
+            extractorInstance = (BaseJoinExtractor) extractorClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException ex) {
+            LOG.error("Could not instantiate extractor " + extractorClass, ex);
+            return;
+        }
+        if (childrenExtractors == null) {
+            childrenExtractors = new HashMap<>();
+        }
+        childrenExtractors.put(clazz, extractorInstance);
+    }
+
+    protected BaseJoinExtractor getExtractor(Class clazz) {
+        if (childrenExtractors == null) {
+            return null;
+        }
+        return childrenExtractors.get(clazz);
     }
 
     protected Map<Class, String> getRowNames() {
@@ -42,6 +82,7 @@ public abstract class BaseJoinExtractor extends ClassExtractor {
             PrimaryKeyJoinColumn joinColumn =
                     (PrimaryKeyJoinColumn) clazz.getAnnotation(PrimaryKeyJoinColumn.class);
             String rowName = table.name() + "." + joinColumn.name();
+            addChildrenExtractor(clazz);
             names.put(clazz, rowName);
         }
         return names;
